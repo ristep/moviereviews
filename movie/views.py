c5310dd8d6f63e069160f2cf0b1fbef3181db07d
ristep <ristepan@gmail.com>
2022-11-19 23:03:53 +0100

    aha

--- movie/views.py
+++ movie/views.py
@@ -4,13 +4,24 @@ from .models import Movie
 
 
 def home(request):
-   searchTerm = request.GET.get('searchMovie')
-   movies = Movie.objects.all()
-   return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies} )
+   searchTerm = request.GET.get("searchMovie")
+   if searchTerm:
+      movies = Movie.objects.filter(title__icontains=searchTerm)
+   else:
+      movies = Movie.objects.all()
+   return render(request, "home.html", {"searchTerm": searchTerm, "movies": movies})
+
+
+# def home(request):
+#    searchTerm = request.GET.get('searchMovie')
+#    movies = Movie.objects.all()
+#    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies} )
+
 
 def about(request):
    return HttpResponse("About Page!")
 
+
 def signup(request):
- email = request.GET.get('email')
- return render(request, 'signup.html', {'email':email})
+   email = request.GET.get("email")
+   return render(request, "signup.html", {"email": email})
